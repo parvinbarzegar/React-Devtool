4bbdedcabfc46b4af07c1b31abd03ed11ff79b0e
parvinbarzegar <71722674+parvinbarzegar@users.noreply.github.com>
2020-11-13 13:46:25 +0100

    finished the react mapping data to components.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Details content={props.tel} />
+        <Details content={props.email} />
       </div>
     </div>
   );
